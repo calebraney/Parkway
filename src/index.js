001dd54e3abc8578a43219a33d41273772aac54c
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2024-04-01 13:57:03 -0700

    added more interactions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { attr, runSplit } from './utilities';
+import { accordion } from './interactions/accordion';
 import { hoverActive } from './interactions/hoverActive';
 import { countUp } from './interactions/countUp';
 import { mouseOver } from './interactions/mouseOver';
@@ -20,6 +21,72 @@ document.addEventListener('DOMContentLoaded', function () {
 
   //////////////////////////////
   //Global Variables
+
+  const navColorScroll = function () {
+    //Elements
+    const NAV = '[data-ix-navcolor="nav"]';
+    const PAGE_WRAP = '.page_wrap';
+    const NAV_DISTANCE = '36'; //in rems
+
+    //Options
+    const theme = 'data-theme';
+    let defaultTheme = 'light';
+    const themes = ['light', 'dark'];
+
+    //get the elements
+    const nav = document.querySelector(NAV);
+    const pageWrap = document.querySelector(PAGE_WRAP);
+    const sections = document.querySelectorAll('section');
+    if (!pageWrap || sections.length === 0) return;
+
+    //utility function to get the theme and return it
+    const getTheme = function (element) {
+      const elementTheme = element.getAttribute(theme);
+      //return the theme if it is a valid theme otherwise returns the default theme
+      if (themes.includes(elementTheme)) {
+        return elementTheme;
+      } else {
+        return defaultTheme;
+      }
+    };
+
+    //utility function to get the theme and return it
+    const applyTheme = function (section, appliedTheme) {
+      const tl = gsap.timeline({
+        defaults: {
+          duration: 0.6,
+          ease: 'power1.out',
+        },
+        scrollTrigger: {
+          trigger: section,
+          start: `top ${NAV_DISTANCE}`,
+          end: `bottom ${NAV_DISTANCE}`,
+          scrub: true,
+          markers: true,
+          onEnter: () => {
+            nav.setAttribute(theme, appliedTheme);
+          },
+          onEnterBack: () => {
+            nav.setAttribute(theme, appliedTheme);
+          },
+        },
+      });
+    };
+
+    //get the page theme
+    const pageTheme = getTheme(pageWrap);
+    //set the default theme to match the page theme
+    defaultTheme = pageTheme;
+
+    //apply the section theme to the nav
+    sections.forEach(function (section) {
+      sectionTheme = getTheme(section);
+      applyTheme(section, sectionTheme);
+    });
+  };
+
+  //////////////////////////////
+  //Animations
   const missionText = function () {
     const WRAP = '[data-ix-mission="wrap"]';
     const TEXT = '[data-ix-mission="text"]';
@@ -63,6 +130,59 @@ document.addEventListener('DOMContentLoaded', function () {
     );
   };
 
+  const approachCTA = function () {
+    const WRAP = '[data-ix-cta="wrap"]';
+    const IMAGE = '[data-ix-cta="image"]';
+    const MASK = '[data-ix-cta="mask"]';
+    const BUTTON = '[data-ix-cta="button"]';
+
+    const wrap = document.querySelector(WRAP);
+    const image = document.querySelector(IMAGE);
+    const mask = document.querySelector(MASK);
+    const button = document.querySelector(BUTTON);
+    if (!wrap || !image || !mask) return;
+
+    //scroll based animation
+    const scrollTl = gsap.timeline({
+      defaults: {
+        duration: 0.6,
+        ease: 'power1.out',
+      },
+      scrollTrigger: {
+        trigger: wrap,
+        start: 'top 70%',
+        end: 'top top',
+        scrub: 1,
+      },
+    });
+    scrollTl.to(mask, {
+      width: '100%',
+      height: '100%',
+      borderRadius: '0px',
+    });
+
+    if (!button) return;
+    //hover event listener for button
+    const hoverTL = gsap.timeline({
+      paused: true,
+      defaults: {
+        duration: 0.6,
+        ease: 'power1.out',
+      },
+    });
+    hoverTL.to(image, {
+      scale: 1.5,
+      duration: 15,
+    });
+
+    button.addEventListener('mouseover', function () {
+      hoverTL.play();
+    });
+    button.addEventListener('mouseout', function () {
+      hoverTL.reverse();
+    });
+  };
+
   //////////////////////////////
   //Control Functions on page load
   const gsapInit = function () {
@@ -78,6 +198,7 @@ document.addEventListener('DOMContentLoaded', function () {
       (gsapContext) => {
         let { isMobile, isTablet, isDesktop, reduceMotion } = gsapContext.conditions;
         // Resuable Animations
+        accordion(gsapContext);
         hoverActive(gsapContext);
         mouseOver(gsapContext);
         parallax(gsapContext);
@@ -86,6 +207,8 @@ document.addEventListener('DOMContentLoaded', function () {
         countUp(gsapContext);
         //Custom Animations
         missionText();
+        approachCTA();
+        // navColorScroll();
         //or control animations globally in match media
         if (!reduceMotion) {
         }
---
+++ src/interactions/accordion.js
@@ -0,0 +1,112 @@
+import { attr, checkBreakpoints } from '../utilities';
+
+export const accordion = function (gsapContext) {
+  //animation ID
+  const ANIMATION_ID = 'accordion';
+  //elements
+  const ACCORDION_WRAP = '[data-ix-accordion="wrap"]';
+  const ACCORDION_ITEM = '[data-ix-accordion="item"]';
+  const ACCORDION_TOP = '[data-ix-accordion="top"]';
+  const ACCORDION_BOTTOM = '[data-ix-accordion="bottom"]';
+
+  //options
+  const OPTION_FIRST_OPEN = 'data-ix-accordion-first-open';
+  const OPTION_ONE_ACTIVE = 'data-ix-accordion-one-active';
+  const OPTION_KEEP_ONE_OPEN = 'data-ix-accordion-keep-one-open';
+  const OPTION_HOVER_OPEN = 'data-ix-accordion-hover';
+  const ACTIVE_CLASS = 'is-active';
+  const accordionLists = gsap.utils.toArray(ACCORDION_WRAP);
+
+  // utility function to open or close accordions
+  const openAccordion = function (item, open = true) {
+    const bottom = item.querySelector(ACCORDION_BOTTOM);
+    //get state of items
+    const state = Flip.getState(...[item, bottom], {
+      //   props: '',
+      nested: true,
+      absolute: true,
+    });
+    if (open === true) {
+      item.classList.add(ACTIVE_CLASS);
+    } else {
+      item.classList.remove(ACTIVE_CLASS);
+    }
+    // animate elements
+    Flip.from(state, {
+      duration: 0.6,
+      ease: 'power1.out',
+      onStart: () => {},
+      onComplete: () => {
+        ScrollTrigger.refresh();
+      },
+    });
+  };
+  ////////////////////////
+  // event logic
+  if (accordionLists.length === 0 || accordionLists === undefined) return;
+  accordionLists.forEach((list) => {
+    //check breakpoints and quit function if set on specific breakpoints
+    let runOnBreakpoint = checkBreakpoints(list, ANIMATION_ID, gsapContext);
+    if (runOnBreakpoint === false) return;
+    // set up conditions for
+    let firstOpen = attr(false, list.getAttribute(OPTION_FIRST_OPEN));
+    let oneActive = attr(false, list.getAttribute(OPTION_ONE_ACTIVE));
+    let keepOneOpen = attr(false, list.getAttribute(OPTION_KEEP_ONE_OPEN));
+    let hoverOnly = attr(false, list.getAttribute(OPTION_HOVER_OPEN));
+    //get the first accordion item and all of the items
+    const accordionItems = list.querySelectorAll(ACCORDION_ITEM);
+    if (accordionItems.length === 0) return;
+    const firstItem = list.firstElementChild;
+    if (firstOpen) {
+      openAccordion(firstItem);
+    }
+    if (!hoverOnly) {
+      // Add event listener for when accordion lists are clicked
+      list.addEventListener('click', function (e) {
+        // check if the clicked element was the top of an accordion and get that accordion
+        const clickedEl = e.target.closest(ACCORDION_TOP);
+        if (!clickedEl) return;
+        // get all the accordions within this list and the active item
+        const clickedItem = clickedEl.closest(ACCORDION_ITEM);
+        // check if the clicked item is already active
+        let clickedItemAlreadyActive = clickedItem.classList.contains(ACTIVE_CLASS);
+        // if item is NOT ACTIVE
+        if (!clickedItemAlreadyActive) {
+          // check if oneActive is True
+          if (oneActive) {
+            // if one active is true loop through each item
+            accordionItems.forEach((item) => {
+              //if item is the current item Open
+              if (item === clickedItem) {
+                openAccordion(item);
+              }
+              //otherwise remove active class and close
+              else {
+                openAccordion(item, false);
+              }
+            });
+          }
+          if (!oneActive) {
+            // if one active is false just set the current item to active and open it
+            openAccordion(clickedItem);
+          }
+        }
+        // if the current item IS ACTIVE and keep one open is false
+        if (clickedItemAlreadyActive && !keepOneOpen) {
+          openAccordion(clickedItem, false);
+        }
+      });
+    }
+    if (hoverOnly) {
+      const accordionItems = list.querySelectorAll(ACCORDION_ITEM);
+      accordionItems.forEach((item) => {
+        item.addEventListener('mouseover', function () {
+          openAccordion(item);
+        });
+        item.addEventListener('mouseout', function () {
+          openAccordion(item, false);
+        });
+      });
+    }
+  });
+};
